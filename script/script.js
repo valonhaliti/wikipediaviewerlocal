9475f87d2726327658c3941c1fe72375020f7a56
Valon Haliti <valon.f.haliti@gmail.com>
2017-09-24 19:02:53 +0200

    added css and javascript files

---
+++ script/script.js
@@ -0,0 +1,5 @@
+document.getElementById('getForm').addEventListener('submit', getWikipediaArticles);
+
+function getWikipediaArticles() {
+    alert(document.getElementById('search-field').value);
+}
