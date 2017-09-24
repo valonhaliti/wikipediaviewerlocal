30781385544d8a9802790de28bebd0ca0d802dba
Valon Haliti <valon.f.haliti@gmail.com>
2017-09-24 21:38:13 +0200

    a lot of changes in css and some in javascript

--- script/script.js
+++ script/script.js
@@ -1,5 +1,39 @@
-document.getElementById('getForm').addEventListener('submit', getWikipediaArticles);
+function fadeIn(el) {
+    el.style.opacity = 0;
 
-function getWikipediaArticles() {
-    alert(document.getElementById('search-field').value);
+    let tick = function() {
+        el.style.opacity = +el.style.opacity + 0.02;
+
+        if (+el.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+        }
+    };
+    tick();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // document is ready
+
+
+    document.getElementById('getForm').addEventListener('submit', getWikipediaArticles);
+
+    function getWikipediaArticles() {
+        alert(document.getElementById('search-field').value);
+    }
+
+    let el = document.getElementById("search-results");
+
+    fadeIn(el);
+
+
+
+
+
+
+
+
+
+
+
+
+});
