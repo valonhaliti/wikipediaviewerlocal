34ba64082297de1bf051767458e13d0883b742ec
Valon Haliti <valon.f.haliti@gmail.com>
2017-09-29 22:45:30 +0200

    page now is dynamic

--- script/script.js
+++ script/script.js
@@ -2,15 +2,32 @@ document.addEventListener("DOMContentLoaded", function() {
     // document is ready
 
     document.getElementById('getForm').addEventListener('submit', getWikipediaArticles);
+    
     function getWikipediaArticles(e) {
         e.preventDefault();
         let el = document.getElementById("search-results");
+        const xhr = new XMLHttpRequest();
+        const link = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='
+            + encodeURIComponent(document.getElementById('search-field').value) +'&format=json';
+
+        xhr.open('GET', link, true);
+        xhr.onload = function () {
+            if (this.status === 200) {
+                const users = JSON.parse(this.responseText).query.search;
+                let output = "";
+                for (let i in users) {
+                    output += '<div class="article">'
+                            + '<h3><a href="http://en.wikipedia.org/?curid=' + users[i].pageid+'">'+
+                                users[i].title +'</a></h3>';
+                    output += "<p>" + users[i].snippet +"</p></div>";
+
+                }
+                document.getElementById('search-results').innerHTML = output;
+            }
+        };
         fadeIn(el);
+        xhr.send();
     }
-
-
-
-
 });
 
 function fadeIn(el) {
